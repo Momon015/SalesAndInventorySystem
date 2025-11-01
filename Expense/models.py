c105f281b564a7b7126f77d9b5c10fb8b21ff0db
Raymond Dela Cruz <dcruz.raymond15@gmail.com>
2025-11-01 09:16:34 +0800

    Forgot to switch ingredient branch so since I already applied the migration I'll just keep the models here intact for main branch

--- Expense/models.py
+++ Expense/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils.text import slugify
+
+from Product.models import TimeStampModel, SlugModel, Category
 
 # Create your models here.
+
+class Material(TimeStampModel, SlugModel):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='materials', null=True, blank=True)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    quantity = models.PositiveIntegerField(default=1)
+    
+    def __str__(self):
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
